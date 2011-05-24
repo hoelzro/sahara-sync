307ea1e4d7812986294d1ef236dff876fe7690f4
Rob Hoelz <rob@hoelz.ro>
2011-05-23 21:56:05 -0500

    Update storage plugin docs

--- lib/SaharaSync/Hostd/Plugin/Store.pm
+++ lib/SaharaSync/Hostd/Plugin/Store.pm
@@ -98,24 +98,40 @@ hash for C<$username> under the key 'password_hash'.
 
 =head2 $store->fetch_blob($user, $blob)
 
-Returns an L<IO::Handle> that contains the contents of
-a blob named C<$blob> for user C<$user>.  If no such
-blob exists, undef is returned.
+Returns a list of two elements; the first is an L<IO::Handle>-like
+object that is tied to the contents of the blob, and the second is
+an opaque identifier that specifies the current revision of the blob.
+If the blob does not exist for the given user, an empty list is returned.
+If this method is called in anything other than list context, a
+L<SaharaSync::X::BadContext> exception is thrown. (This is mainly to prevent
+calls to this method that leak information; this may be changed in the future)
+If C<$user> does not exist, a L<SaharaSync::X::BadUser> exception is thrown.
 
-=head2 $store->store_blob($user, $blob, $handle)
-
-=head2 $store->store_blob($user, $blob)
+=head2 $store->store_blob($user, $blob, $handle, $revision)
 
 Stores the contents of C<$handle> in a blob called C<$blob>
 for user C<$user>.  C<$handle> is NOT necessarily an L<IO::Handle>;
 the only interface it must implement is the read method
-(see L<PSGI/"The Input Stream"> for details).  If C<$handle> is
-undef or omitted, that blob is deleted.
+(see L<PSGI/"The Input Stream"> for details).  If creating a new blob,
+C<$revision> must not be specified (or specified as undef); otherwise,
+the current revision of the blob should be provided.  If the storage operation
+is successful (the blob is new, or the provided revision matches the current),
+a new opaque revision identifer is returned; otherwise, undef is returned.
 
-=head2 $store->fetch_changed_blobs($user, $timestamp)
+=head2 $store->delete_blob($user, $blob, $revision)
 
-Returns an array of blob names that have changed since C<$timestamp>.
-Duplicate entries may or may not be included.
+Deletes the blob C<$blob> for user C<$user>.  If the blob does not exist, a
+L<SaharaSync::X::NoSuchBlob> exception is thrown.  C<$revision> should be the
+current revision of the blob.  If the delete operation is successful (the
+revisions match), a new opaque revision identifer is returned; otherwise,
+undef is returned.
+
+=head2 $store->fetch_changed_blobs($user, $since_revision)
+
+Returns an array of unique blob names that have changed since
+C<$since_revision>.  C<$since_revision> may be omitted; in that case, all
+blobs for the given user are returned.  If C<$since_revision> is not a known
+revision, a L<SaharaSync::X::BadRevision> exception is thrown.
 
 =head1 AUTHOR
 

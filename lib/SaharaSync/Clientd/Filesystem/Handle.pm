bd22f142864cfb6523f350ca84446f3eebbe2d3b
Rob Hoelz <rob@hoelz.ro>
2011-07-27 00:59:25 -0500

    Add basic filesystme object implementation

---
+++ lib/SaharaSync/Clientd/Filesystem/Handle.pm
@@ -0,0 +1,38 @@
+package SaharaSync::Clientd::Filesystem::Handle;
+
+use strict;
+use warnings;
+use parent 'IO::Handle';
+
+sub new {
+    my ( $class, $tempfile, $mode, $destination ) = @_;
+
+    my $self = IO::Handle::new_from_fd($class, fileno($tempfile), 'w');
+    @{*$self}{qw/temp_name mode real_name/} =
+        ( $tempfile->filename, $mode, $destination );
+    return $self;
+}
+
+sub close {
+    my ( $self ) = @_;
+
+    my ( $temp_name, $mode, $real_name ) = @{*$self}{qw/temp_name mode real_name/};
+
+    my $retval = IO::Handle::close($self);
+    chmod $mode, $temp_name or die $! if defined $mode;
+    rename $temp_name, $real_name or die $!;
+
+    return $retval;
+}
+
+1;
+
+__END__
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 FUNCTIONS
+
+=cut

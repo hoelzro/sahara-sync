1fe2e5880ee8dcaca2811199bb3c3e18b815cc0e
Rob Hoelz <rob@hoelz.ro>
2011-08-17 18:00:36 -0500

    Implement install_exception_handler

---
+++ lib/SaharaSync/Util.pm
@@ -0,0 +1,47 @@
+package SaharaSync::Util;
+
+use strict;
+use warnings;
+
+sub install_exception_handler {
+    my ( $class, $handler ) = @_;
+
+    $SIG{__DIE__} = sub {
+        my ( $message ) = @_;
+
+        my $i = 0;
+
+        my @last_eval_frame;
+
+        while(my @info = caller($i)) {
+            my ( $subroutine, $evaltext ) = @info[3, 6];
+
+            if($subroutine eq '(eval)' && !defined($evaltext)) {
+                @last_eval_frame = caller($i + 1);
+                last;
+            }
+        } continue {
+            $i++;
+        }
+
+        if(@last_eval_frame) {
+            my $subroutine = $last_eval_frame[3];
+
+            if($subroutine =~ /^AnyEvent::Impl/) {
+                $handler->($message);
+            }
+        }
+    };
+}
+
+1;
+
+__END__
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 FUNCTIONS
+
+=cut

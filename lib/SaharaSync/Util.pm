3780a85f7e27500811c863f959a2f0c83814c5f1
Rob Hoelz <rob@hoelz.ro>
2011-08-17 20:03:26 -0500

    Add load_logger to SaharaSync::Util

--- lib/SaharaSync/Util.pm
+++ lib/SaharaSync/Util.pm
@@ -3,7 +3,10 @@ package SaharaSync::Util;
 use strict;
 use warnings;
 
-require Carp;
+use Carp qw(croak);
+use Log::Dispatch;
+use namespace::clean;
+
 $Carp::CarpInternal{ (__PACKAGE__) } = 1;
 
 sub install_exception_handler {
@@ -37,6 +40,43 @@ sub install_exception_handler {
             }
         }
     };
+}
+
+sub load_logger {
+    my ( $class, $configs ) = @_;
+
+    my @messages;
+    my @loggers;
+
+    foreach my $config (@$configs) {
+        my ( $type, $mandatory ) = delete @{$config}{qw/type mandatory/};
+
+        my $pristine_type = $type;
+        $type             = 'Log::Dispatch::' . $type unless $type =~ s/^\+//;
+        my $path          = $type;
+        $path             =~ s!::!/!g;
+        $path            .= '.pm';
+
+        eval {
+            require $path;
+            push @loggers, $type->new(%$config);
+        };
+        if($@) {
+            if($mandatory) {
+                croak "Unable to load mandatory logger '$pristine_type': $@";
+            } else {
+                push @messages, "Unable to load logger '$pristine_type': $@";
+            }
+        }
+    }
+
+    croak "Unable to load any loggers" unless @loggers;
+
+    my $logger = Log::Dispatch->new;
+    $logger->add($_)  foreach @loggers;
+    $logger->warn($_) foreach @messages;
+
+    return $logger;
 }
 
 1;

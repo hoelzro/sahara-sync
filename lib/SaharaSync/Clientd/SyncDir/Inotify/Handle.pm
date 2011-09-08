b93b5b19cb861fce5a76276b63979ddfbd9daa09
Rob Hoelz <rob@hoelz.ro>
2011-09-07 22:34:22 -0500

    Switch inotify handles to File::Temp objects
    Not path strings

--- lib/SaharaSync/Clientd/SyncDir/Inotify/Handle.pm
+++ lib/SaharaSync/Clientd/SyncDir/Inotify/Handle.pm
@@ -10,26 +10,28 @@ sub new {
     my ( $class, $sync_dir, $tempfile, $mode, $destination ) = @_;
 
     my $self = IO::Handle::new_from_fd($class, fileno($tempfile), 'w');
-    @{*$self}{qw/temp_name mode real_name sync_dir/} =
-        ( $tempfile->filename, $mode, $destination, $sync_dir );
+    close $tempfile;
+    @{*$self}{qw/tempfile mode real_name sync_dir/} =
+        ( $tempfile, $mode, $destination, $sync_dir );
+
     return $self;
 }
 
 sub cancel {
     my ( $self ) = @_;
 
-    my ( $temp_name ) = ${*$self}{'temp_name'};
-    unlink $temp_name or warn $!;
+    my $tempfile = ${*$self}{'tempfile'};
+    unlink $tempfile->filename or warn $!;
 }
 
 sub close {
     my ( $self ) = @_;
 
-    my ( $temp_name, $mode, $real_name, $sync_dir ) = @{*$self}{qw/temp_name mode real_name sync_dir/};
+    my ( $tempfile, $mode, $real_name, $sync_dir ) = @{*$self}{qw/tempfile mode real_name sync_dir/};
 
     my $retval = IO::Handle::close($self);
-    chmod $mode, $temp_name or die $! if defined $mode;
-    rename $temp_name, $real_name or die $!;
+    chmod $mode, $tempfile->filename or die $! if defined $mode;
+    rename $tempfile->filename, $real_name or die $!;
 
     my $file = File::Spec->abs2rel($real_name, $sync_dir->root);
     $sync_dir->_update_file_stats($real_name, $file);

254d7e3fbd797d8e23955967d1fed3dbde24b4f7
Rob Hoelz <rob@hoelz.ro>
2011-09-20 21:26:14 -0500

    Add timeout functionality to Test::Class::AnyEvent

--- t/lib/Test/Class/AnyEvent.pm
+++ t/lib/Test/Class/AnyEvent.pm
@@ -7,6 +7,36 @@ use parent 'Test::Class';
 use EV;
 use Test::More;
 
+my %timeouts;
+
+sub TIMEOUT {
+    my $self  = shift;
+    my $class = ref($self) || $self;
+
+    if(@_) {
+        $timeouts{$class} = shift;
+    }
+    return $timeouts{$class} || 30;
+}
+
+sub setup_kill_timeout :Test(setup) {
+    my ( $self ) = @_;
+
+    $self->{'kill_timer'} = AnyEvent->timer(
+        after => $self->TIMEOUT,
+        cb    => sub {
+            diag "Your test took too long!";
+            exit 1;
+        },
+    );
+}
+
+sub stop_kill_timeout :Test(teardown) {
+    my ( $self ) = @_;
+
+    delete $self->{'kill_timer'};
+}
+
 sub setup_error_handler :Test(setup) {
     my ( $self ) = @_;
 

962d0f4ed486c49ad792ad09e855eb89e87c5dfd
Rob Hoelz <rob@hoelz.ro>
2011-10-04 17:59:35 -0500

    Add SaharaSync::Hostd::Server

---
+++ lib/SaharaSync/Hostd/Server.pm
@@ -0,0 +1,44 @@
+package SaharaSync::Hostd::Server;
+
+use Moose;
+use Twiggy::Server;
+
+use namespace::clean -except => 'meta';
+
+has twiggy => (
+    is => 'ro'
+);
+
+sub BUILDARGS {
+    my ( $self, %args ) = @_;
+
+    return {
+        twiggy => Twiggy::Server->new(%args),
+    };
+}
+
+sub start {
+    my ( $self, $app ) = @_;
+
+    $self->twiggy->register_service($app);
+}
+
+sub stop {
+    my ( $self ) = @_;
+
+    $self->twiggy(undef);
+}
+
+__PACKAGE__->meta->make_immutable;
+
+1;
+
+__END__
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 FUNCTIONS
+
+=cut

6bbaafc6f3735d4f29f8875bdcb1cbc2a4b6e51e
Rob Hoelz <rob@hoelz.ro>
2011-10-04 23:47:54 -0500

    Add SaharaSync hostd launcher class

---
+++ lib/SaharaSync/Hostd/Launcher.pm
@@ -0,0 +1,58 @@
+package SaharaSync::Hostd::Launcher;
+
+use strict;
+use warnings;
+
+use AnyEvent;
+use SaharaSync::Hostd;
+use SaharaSync::Hostd::Server;
+
+use namespace::clean -except => 'meta';
+
+sub run {
+    my ( undef, $config ) = @_;
+
+    my $hostd  = SaharaSync::Hostd->new($config);
+    my $app    = $hostd->to_app;
+    my $server = SaharaSync::Hostd::Server->new(
+        port => $hostd->port,
+    );
+    $server->start;
+
+    my $cond = AnyEvent->condvar;
+
+    my $term = AnyEvent->signal(
+        signal => 'TERM',
+        cb     => sub {
+            $cond->send;
+        },
+    );
+
+    my $int = AnyEvent->signal(
+        signal => 'INT',
+        cb     => sub {
+            $cond->send;
+        },
+    );
+
+    my $pipe = AnyEvent->signal(
+        signal => 'PIPE',
+        cb     => sub {},
+    );
+
+    $hostd->log->info('Starting host daemon');
+    $cond->recv;
+    $server->stop;
+}
+
+1;
+
+__END__
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 FUNCTIONS
+
+=cut

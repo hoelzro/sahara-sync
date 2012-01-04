f73ee3f44bb77a0b01c07dea12e9bd72014ec828
Rob Hoelz <rob@hoelz.ro>
2012-01-03 23:35:12 -0600

    Fix up temp files script

--- dev/temp-files.pl
+++ dev/temp-files.pl
@@ -6,28 +6,16 @@ use feature 'say';
 
 use File::Slurp qw(read_dir);
 
-my @test_methods = map { chomp; $_ } <DATA>;
+my @test_methods = map { chomp; $_ } qx(perl dev/print-test-class-methods.pl t/client/sync-streaming.t);
 
 $ENV{'TMPDIR'} = '/tmp/sahara';
 
 foreach my $method (@test_methods) {
     $ENV{'TEST_METHOD'} = $method;
 
+    mkdir '/tmp/sahara';
     system 'prove t/client/sync-streaming.t >/dev/null 2>&1';
     my @temp_files = read_dir('/tmp/sahara');
     system 'rm -rf /tmp/sahara/*';
     say $method if @temp_files;
 }
-
-__DATA__
-test_create_conflict
-test_create_file
-test_delete_file
-test_delete_update_conflict
-test_offline_update
-test_preexisting_files
-test_revision_persistence
-test_update_conflict
-test_update_delete_conflict
-test_update_file
-test_update_on_nonorigin

995e5c3d90fda2381f5b684455771ac28e37689f
Rob Hoelz <rob@hoelz.ro>
2012-02-08 20:29:32 -0600

    Allow Test::Sahara::Hostd to accept options

--- t/lib/Test/Sahara/Host.pm
+++ t/lib/Test/Sahara/Host.pm
@@ -5,12 +5,15 @@ use warnings;
 use parent 'Test::Sahara::ChildProcess';
 
 sub new {
-    my ( $class ) = @_;
+    my ( $class, %options ) = @_;
 
     return Test::Sahara::ChildProcess::new($class, sub {
         my ( $port ) = @_;
 
         $ENV{'_HOSTD_PORT'} = $port;
+        if($options{'disable_streaming'}) {
+            $ENV{'_HOSTD_DISABLE_STREAMING'} = 1;
+        }
 
         exec $^X, 't/run-test-app';
     });

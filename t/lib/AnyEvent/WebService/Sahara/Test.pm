1ca1bbc518dd59098d86629353e6a735ff9b43d6
Rob Hoelz <rob@hoelz.ro>
2012-02-11 16:28:42 -0600

    Restore the connection error test
    This reverts commit 59e02874d272dd63a03d0919ea7c0433e9984289.

--- t/lib/AnyEvent/WebService/Sahara/Test.pm
+++ t/lib/AnyEvent/WebService/Sahara/Test.pm
@@ -1365,8 +1365,6 @@ sub test_empty_blob :Test(2) {
 sub test_unavailable_errors :Test(1) {
     my ( $self ) = @_;
 
-    return 'for now';
-
     my $proxy       = Test::Sahara::Proxy->new(remote => $self->port);
     my $client      = $self->create_client($proxy->port);
     my $errors_seen = 0;
